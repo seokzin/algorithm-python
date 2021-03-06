4ec9e1d830c81b852583111ffa3d538f0f372454
seokzin <seokzin95@gmail.com>
2021-03-06 20:37:34 +0900

    210306 TIL (Notebook)

--- Code/Backjoon/임시-1018-체스판다시칠하기.py
+++ Code/Backjoon/임시-1018-체스판다시칠하기.py
@@ -1,18 +1,26 @@
 n, m = map(int, input().split())
 
-arr = [] # 체스판 담을 배열
+arr = [] # 알파벳 체스판
 
 for i in range(n):
-  arr.append(input())
+  arr.append(list(map(str, input())))
 
-cnt = 0 # 틀린 무늬 카운트
+def comp(arr, x, y):
+  dif = arr # arr 복사. 차이값을 숫자로 드러내자. 매번 초기화 해야함.
 
-# 아이디어: w = 0 b = 1로 치환해서 원본과 가본을 뺀다. 0을 카운트하거나 1, -1을 카운트한거의 min값을 채택한다.
-
-# ex)시작 좌표 x, y
-def compare(arr, x, y): 
   for i in range(x, x+8):
-    if arr[(i%2 == 1)] == 'W':
-      continue
+    for j in range(y, y+8):
+      if (i+j) % 2 == 0: # 첫 블록 포함 띄엄띄엄
+        dif[j][i] = 1 if arr[j][i] == 'W' else 0
+      else: # 둘째부터 띄엄띄엄
+        dif[j][i] = 1 if arr[j][i] == 'B' else 0
 
-# 1920번 어렵네. 나중에 다시 풀자
+  dif_num = sum(sum(dif, []))
+
+  print(min(dif_num, 64-dif_num))
+
+for y in range(n-8):
+  for x in range(m-8):
+    comp(arr, x, y)
+
+# 2d 리스트는 count, sum이 안된다. 2번 나눠야 한다. 대신 sum은 sum(sum(arr, []))로 가능
