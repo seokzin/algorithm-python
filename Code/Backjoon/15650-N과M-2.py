bd446b7d71166ec5a2fcd36ca27067985ee59c1e
seokzin <seokzin95@gmail.com>
2021-05-17 23:29:26 +0900

    210517 TIL

--- Code/Backjoon/15650-N과M-2.py
+++ Code/Backjoon/15650-N과M-2.py
@@ -7,12 +7,12 @@ def dfs(start):
     return
   
   for i in range(start, n+1):
-    if i not in s:
-      s.append(i)
-      dfs(i+1)
-      s.pop()
+    # if i not in s:
+    s.append(i)
+    dfs(i+1)
+    s.pop()
 
 dfs(1)
 
 # 15649와 비슷하니 묶어서 생각
-# not in은 if visited와 비슷한 기능하는 괜찮은 문법인듯
+# not in은 if visited와 비슷한 기능하는 괜찮은 문법인듯. 다만 여기선 안써도 결과 같음
---
+++ Code/Backjoon/15652-N과M-4.py
@@ -0,0 +1,14 @@
+n, m = map(int, input().split())
+s = []
+
+def dfs(start):
+  if len(s) == m:
+    print(*s)
+    return
+  
+  for i in range(start, n+1):
+    s.append(i)
+    dfs(i)
+    s.pop()
+
+dfs(1)
