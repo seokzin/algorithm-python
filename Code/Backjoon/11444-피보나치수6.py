11c86e9a0b09b1b9d7dfeb1582389bbb4db077f4
seokzin <seokzin95@gmail.com>
2021-06-09 22:56:56 +0900

    210609 TIL

--- Code/Backjoon/11444-피보나치수6.py
+++ Code/Backjoon/11444-피보나치수6.py
@@ -1,9 +1,12 @@
+# 행렬의 거듭제곱을 이용한 피보나치 구하기 방식
+
 n = int(input())
-mat = [[1, 1], [1, 0]]
+
+mat = [[1, 1], [1, 0]] # 제곱될 행렬
 res = [[1, 0], [0, 1]]
 
-def multi_mat(a, b):
-  mul = [[0] * 2 for _ in range(2)]
+def multi(a, b):
+  mul = [[0] * 2 for _ in range(2)] # 2x2 배열 초기화
 
   for i in range(2):
     for j in range(2):
@@ -17,11 +20,12 @@ def multi_mat(a, b):
   return mul
   
 while n:
-  if n & 1: 
-    res = multi_mat(res, mat)
-  mat = multi_mat(mat, mat)
-  n //= 2
+  if n % 2: # 나머지 1 일때 
+    res = multi(res, mat) # 제곱수가 홀수일 때 분할을 통해 짝수로 만들어준다.
+  mat = multi(mat, mat) 
+  n //= 2 # 제곱 횟수를 log 복잡도로 줄이는 과정
 
 print(res[1][0])
 
-# 20) 무슨 문법?
+# 수학적 지식 - 피보나치를 행렬곱으로
+# 센스 - (22~26)제곱을 log 복잡도로 줄이는 방법
