c49e3c404c0db279d640eadade29636587178183
seokzin <seokzin95@gmail.com>
2021-06-30 22:04:35 +0900

    210630 TIL

--- Code/Backjoon/note.py
+++ Code/Backjoon/note.py
@@ -1,16 +1,20 @@
+import sys
+
 n = int(input())
 arr = list(map(int, input().split()))
-ans = []
+
 l, r = 0, n-1
+ans = [abs(arr[l]+arr[r]), (arr[l],arr[r])]
+
 
 while l < r:
-  v = arr[l] + arr[r]
+  mix = arr[l] + arr[r]
 
   if abs(mix) < ans[0]:
     ans[0] = abs(mix)
-    ans[1] = (liq[l],liq[r])
+    ans[1] = (arr[l], arr[r])
   if mix > 0:
-    r-=1
+    r -= 1
   elif mix < 0:
     l += 1
   else:
