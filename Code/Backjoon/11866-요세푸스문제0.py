203a3221c617081668bac4b6d7af5dde6fda2a65
seokzin <seokzin95@gmail.com>
2021-07-27 11:40:53 +0900

    [BOJ/11866] 요세푸스 문제 0 (S4)
    - 기존 코드 가독성 리팩토링

--- Code/Backjoon/11866-요세푸스문제0.py
+++ Code/Backjoon/11866-요세푸스문제0.py
@@ -1,18 +1,16 @@
 n, k = map(int, input().split())
 
-arr_a = list(range(1,n+1))
+arr_a = [i for i in range(1,n+1)]
 arr_k = [] # 사망자
 idx = 0
 
-while len(arr_a) > 0:
-  idx = (idx + (k-1)) % n # 3 6 2 5
+while arr_a:
+  idx = (idx + (k-1)) % n
   n -= 1
 
-  arr_k.append(arr_a[idx])
-  arr_a.remove(arr_a[idx])
+  arr_k.append(arr_a.pop(idx))
 
 print("<" + ", ".join(list(map(str, arr_k))) + ">")
 
-# len(arr) 대신 arr[-1]을 활용한다..
-# 1. 반례) n이 1일 때. <1, > 으로 출력됐다.
-# join() - 리스트 사이에 기호를 넣어 문자열로 표현. 성능 차이는 거의 X. (이를 통해 간단화, n=1 반례 해결!). 다만 join의 변수는 str이여야 한다. https://hyesun03.github.io/2017/04/08/python_int_join/
+# join() - 리스트 사이에 기호를 넣어 문자열로 표현. 다만 join의 변수는 str
+# 또는 *arr_k, sep=", " 를 활용할 수도 있다
