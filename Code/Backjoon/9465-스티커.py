6ae558e8de90dc120f0b24a7941d45d17acfc3a5
seokzin <seokzin95@gmail.com>
2021-07-27 18:26:46 +0900

    [BOJ/9465] 스티커 (S2)
    - n = 1 일 때 예외처리가 필요함
    - 이전엔 맞았던 문제. 케이스가 추가되고 틀려서 코드 리팩토링
    - 반례 찾느라 굉장히 애먹었다

--- Code/Backjoon/9465-스티커.py
+++ Code/Backjoon/9465-스티커.py
@@ -1,17 +1,18 @@
-for _ in range(int(input())):
+t = int(input())
+
+for _ in range(t):
   n = int(input())
   dp = []
 
   for _ in range(2):
     dp.append(list(map(int, input().split())))
 
-  dp[0][1] += dp[1][0]
-  dp[1][1] += dp[0][0]
+  if n > 1:  # n=1일때 index 에러 발생
+    dp[0][1] += dp[1][0]
+    dp[1][1] += dp[0][0]
 
   for i in range(2, n):
     dp[0][i] += max(dp[1][i-1], dp[1][i-2])
     dp[1][i] += max(dp[0][i-1], dp[0][i-2])
 
-  print(max(dp[0][n-1], dp[1][n-1]))
-
-  # list 없이 map만 하고 append 하면 뭐가 다른걸까
+  print(max(dp[0][-1], dp[1][-1]))
