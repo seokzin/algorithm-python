14bf7f93aa48a968d82c2e4b0346e4f6c511ac21
seokzin <seokzin95@gmail.com>
2021-07-27 12:07:42 +0900

    [BOJ/11866] 요세푸스 문제 0 (S4)
    - 출력부 f-string 으로 리팩토링

--- Code/Backjoon/11866-요세푸스문제0.py
+++ Code/Backjoon/11866-요세푸스문제0.py
@@ -5,12 +5,11 @@ arr_k = [] # 사망자
 idx = 0
 
 while arr_a:
-  idx = (idx + (k-1)) % n
-  n -= 1
-
+  idx = (idx + (k-1)) % len(arr_a)
   arr_k.append(arr_a.pop(idx))
 
-print("<" + ", ".join(list(map(str, arr_k))) + ">")
+print(f"<{', '.join(map(str, arr_k))}>")
 
+# 출력부가 까다로운 문제
 # join() - 리스트 사이에 기호를 넣어 문자열로 표현. 다만 join의 변수는 str
 # 또는 *arr_k, sep=", " 를 활용할 수도 있다
