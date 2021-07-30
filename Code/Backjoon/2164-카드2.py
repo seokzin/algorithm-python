407db1cf416c084a8ba432dacb9965e1cd1eabb9
seokzin <seokzin95@gmail.com>
2021-07-30 19:24:10 +0900

    [BOJ/2164] 카드2 (S4)
    - len() 메서드의 반복 제거 -> 아주 조금의 성능 향상

--- Code/Backjoon/2164-카드2.py
+++ Code/Backjoon/2164-카드2.py
@@ -2,15 +2,12 @@ from collections import deque
 
 n = int(input())
 
-deq = deque()
-for i in range(1, n+1):
-  deq.append(i)
+deq = deque(i for i in range(1, n+1))
 
-while len(deq) > 1:
-  deq.popleft() # 윗 카드 버리기
-  deq.append(deq.popleft()) # 윗 카드를 제일 밑으로
+for _ in range(n-1):
+  deq.popleft()
+  deq.append(deq.popleft())
 
 print(deq[0])
 
-# 1. 시간초과 - 직접 만들지 말고 큐를 써야겠다. 근데 remove와 append의 성능 차가 어떻길래? 정리하자. https://chaewonkong.github.io/posts/python-deque.html
-# 덱과 큐의 차이는? 덱은 양방향 큐다. 
+# 어차피 n-1번 반복하기 때문에 while len(deq) > 1 보다 for(n-1)이 성능 좋다
