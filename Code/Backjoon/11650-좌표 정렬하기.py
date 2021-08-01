149bd6a6011404cbd95933d9a29f4d994d1cb2c7
seokzin <seokzin95@gmail.com>
2021-08-02 00:42:33 +0900

    [BOJ/11650] 좌표 정렬하기 (S5)
    - 람다 연습하기 좋은 문제
    - 사실 그냥 sort() 해도 2차원 리스트 정렬 가능함

---
+++ Code/Backjoon/11650-좌표 정렬하기.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+dots = []
+
+for _ in range(n):
+  dot = list(map(int, input().split()))
+  dots.append(dot)
+
+dots.sort(key=lambda x: (x[0], x[1]))  # x정렬 후 y정렬
+# dots.sort()
+
+for dot in dots:
+  print(*dot)
+
+# 사실 sort()로 해도 x정렬 -> y정렬 해준답니다
