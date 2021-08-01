b244d1c4d6a8e94fa2234dc793ec5afc8622561b
seokzin <seokzin95@gmail.com>
2021-08-01 17:57:00 +0900

    [BOJ/10814] 나이순 정렬 (S5)
    - readline의 성능 차이를 크게 느낄 수 있었다

---
+++ Code/Backjoon/10814-나이순 정렬.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+members = []
+
+for _ in range(n):
+  age, name = map(str, input().split())
+  members.append((int(age), name))
+
+members.sort(key=lambda x: x[0]) 
+
+for member in members:
+  print(*member)
+
+# write()는 readline()에 비해 극적인 성능차이가 없다고 생각합니다
