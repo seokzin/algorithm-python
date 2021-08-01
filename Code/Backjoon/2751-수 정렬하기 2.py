a2e85dd5f74ae78a9469a6b8cb2eb5c45aef6672
seokzin <seokzin95@gmail.com>
2021-08-01 16:38:57 +0900

    [BOJ/2751] 수 정렬하기 2 (S5)
    - input 양이 많은게 난관인 문제
    - PyPy3을 고려한다는게 좋은 문제란 생각이 안듦

---
+++ Code/Backjoon/2751-수 정렬하기 2.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+nums = [int(input()) for _ in range(n)]
+
+nums.sort()
+
+# print(*nums, sep='\n')
+
+for num in nums:
+  sys.stdout.write(str(num) + '\n')
+
+# 파이썬 정렬 내장 메서드 -> O(nlogn)
+# 1. 시간 초과 - PyPy3으로 바꾸고 성공 (메모리는 거의 3배)
+# 2. 시간 단축 - readline 도입시 1732ms -> 840ms
+# 3. 시간 단축 - write 도입시 840ms -> 760ms
