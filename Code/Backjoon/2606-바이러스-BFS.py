8332d31ec634d487db93480891b70ffeb4d24c85
seokzin <seokzin95@gmail.com>
2021-08-09 01:13:16 +0900

    [BOJ/2606] 바이러스 (S3)
    - BFS, DFS 기본 문제

--- Code/Backjoon/2606-바이러스-BFS.py
+++ Code/Backjoon/2606-바이러스-BFS.py
@@ -1,32 +1,29 @@
 from collections import deque
 
+def bfs(n):
+  q = deque()
+  q.append(n)
+
+  while q:
+    x = q.popleft()
+
+    if not visit[x]:
+      visit[x] = 1
+
+      for nx in graph[x]:
+        q.append(nx)
+
 n = int(input())
 k = int(input())
 
-graph = [[] for _ in range(n + 1)]
-visit = [0] * (n + 1)
+graph = [[] for _ in range(n+1)]
+visit = [0] * (n+1)
 
 for _ in range(k):
   a, b = map(int, input().split())
   graph[a].append(b)
   graph[b].append(a)
 
-def bfs(n):
-  q = deque()
-  q.append(n)
-
-  while q:
-    i = q.popleft()
-
-    if visit[i] == 0:
-      visit[i] = 1
-
-      for j in graph[i]:
-        q.append(j)
-
 bfs(1)
 
-print(sum(visit) - 1)
-
-# 전체적으로 훑는게 BFS가 적합하다던데.. 내 코드는 BFS가 DFS보다 더 느렸다.
-# 덱을 선언할 때 q로 하는게 좋지 않은 습관? 대부분 queue로 하는듯
+print(sum(visit)-1)  # 1번 컴퓨터 제외
--- Code/Backjoon/2606-바이러스-DFS.py
+++ Code/Backjoon/2606-바이러스-DFS.py
@@ -1,24 +1,21 @@
+def dfs(n):
+    if not visit[n]:
+        visit[n] = 1
+
+        for j in graph[n]:
+            dfs(j)
+
 n = int(input())
 k = int(input())
 
-graph = [[] for _ in range(n + 1)] # 인덱스 맞추기 위해
-visit = [0] * (n + 1)
+graph = [[] for _ in range(n+1)]
+visit = [0] * (n+1)
 
 for _ in range(k):
   a, b = map(int, input().split())
   graph[a].append(b)
   graph[b].append(a)
 
-def dfs(n):
-  if visit[n] == 0:
-    visit[n] = 1
-
-    for j in graph[n]:
-      dfs(j)
-
 dfs(1)
 
-print(sum(visit) - 1) # visit[1]은 뺀다.
-
-# 그래프를 이중 리스트로 초기화하는 사례들은 무슨 이유?
-# 함수에서 else를 생략하면 자동 return일까?
+print(sum(visit)-1)  # 1번 컴퓨터 제외
