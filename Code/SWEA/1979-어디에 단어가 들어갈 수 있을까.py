e402d227d16099ea7ddbfd12e35203888ee033b6
seokzin <seokzin95@gmail.com>
2021-08-12 15:28:20 +0900

    [SWEA/1979] 어디에 단어가 들어갈 수 있을까

---
+++ Code/SWEA/1979-어디에 단어가 들어갈 수 있을까.py
@@ -0,0 +1,31 @@
+T = int(input())
+
+for tc in range(1, T+1):
+    N, K = map(int, input().split())
+
+    arr = [list(map(int, input().split())) + [0] for _ in range(N)]
+    arr.append([0]*(N+1))  # 0을 더해주는 이유 - 끝값이 1이면 else문을 통해 cnt 검증을 하지 못해서
+
+    res = 0
+
+    for a in range(N):
+        cnt = 0
+
+        for b in range(N+1):  # 가로 순회
+            if arr[a][b]:
+                cnt += 1
+
+            else:
+                if cnt == K:
+                    res += 1
+                cnt = 0
+
+        for b in range(N+1):  # 세로 순회
+            if arr[b][a]:
+                cnt += 1
+            else:
+                if cnt == K:
+                    res += 1
+                cnt = 0
+
+    print(f"#{tc} {res}")
