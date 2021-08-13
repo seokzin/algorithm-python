90b80ab1d361e564fb932c176668d67c2e977982
seokzin <seokzin95@gmail.com>
2021-08-13 13:09:42 +0900

    [SWEA/L4837] 부분집합의 합 (refactor)

--- Code/SWEA/L4837-부분집합의 합.py
+++ Code/SWEA/L4837-부분집합의 합.py
@@ -1,21 +1,21 @@
 def subset(x):
     global cnt
 
-    if x == 12:  # 끝까지 탐색함
+    if x > 12:  # 끝까지 탐색함
         if sum(bit) == N:  # 부분집합 개수가 N개라면
             sub_sum = 0
 
-            for i in range(12):
+            for i in range(1, 13):
                 if bit[i]:
-                    sub_sum += i+1  # 인덱스 보정
+                    sub_sum += i
 
             if sub_sum == K:
                 cnt += 1
     else:
-        bit[x] = 0
+        bit[x] = 1
         subset(x+1)
 
-        bit[x] = 1
+        bit[x] = 0
         subset(x+1)
 
 
@@ -23,10 +23,10 @@ T = int(input())
 
 for tc in range(1, T+1):
     N, K = map(int, input().split())
-    bit = [0] * 12  # 부분집합에 해당 원소의 존재 상태 나타냄 (1, 0)
+    bit = [0] * 13  # 부분집합에 해당 원소의 존재 상태 나타냄 (1, 0)
     cnt = 0
 
-    subset(0)
+    subset(1)
 
     print(f"#{tc} {cnt}")
 
