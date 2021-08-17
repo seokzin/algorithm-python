32121c15657deb5e9630e25afa4d0354b8b8df4a
seokzin <seokzin95@gmail.com>
2021-08-17 23:37:35 +0900

    [SWEA/4861] 회문 (add comments)

--- Code/SWEA/4861-회문.py
+++ Code/SWEA/4861-회문.py
@@ -2,28 +2,25 @@ T = int(input())
 
 for tc in range(1, T+1):
     N, M = map(int, input().split())
-    table = []
-
-    for _ in range(N):
-        table.append(input())
+    arr = [input() for _ in range(N)]
 
     # 가로 탐색
     for y in range(N):
         for x in range(N-M+1):
-            row = table[y][x:M+x]  
+            row = arr[y][x:M+x]  
             
             if row == row[::-1]:  
                 print(f"#{tc}", row)
-
 
     # 세로 탐색
     for y in range(N-M+1):
         for x in range(N):
             col = []
             for i in range(M):
-                col.append(table[y+i][x])
+                col.append(arr[y+i][x])
 
             if col == col[::-1]:  
                 print(f"#{tc} ", *col, sep='')
 
-# 무식하게 검증 했습니다.
+# 회문1은 전치행렬 없이 인덱싱으로 풀었습니다.
+# 회문2는 전치행렬을 사용해봤습니다. 확실히 편합니다.
