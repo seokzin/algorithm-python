445bb954905505fb3fe65b00c8491d4c82c56e3e
seokzin <seokzin95@gmail.com>
2021-08-19 14:00:54 +0900

    [SWEA/2005] 파스칼의 삼각형 (Add comment)

--- Code/SWEA/2005-파스칼의 삼각형-2.py
+++ Code/SWEA/2005-파스칼의 삼각형-2.py
@@ -1,10 +1,11 @@
 for tc in range(1, int(input())+1):
-    N = int(input())
-    
     print(f'#{tc}')
     
-    for i in range(N):
+    for i in range(int(input())):
         row = [1]
         for j in range(1, i+1):
             row.append(int(row[-1]*((i-j+1)/j)))
+
         print(*row)
+
+# 조합 식을 응용해 풀었습니다.
