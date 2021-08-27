13980fae2772308f0ee68f8654f15d88eb1dc70e
seokzin <seokzin95@gmail.com>
2021-08-27 19:28:16 +0900

    [BOJ/9095] 1, 2, 3 더하기 (S3)

--- Code/Backjoon/9095-1, 2, 3 더하기.py
+++ Code/Backjoon/9095-1, 2, 3 더하기.py
@@ -1,15 +1,9 @@
-def dp(n):
-  if n == 1:
-    return 1
-  elif n == 2:
-    return 2
-  elif n == 3:
-    return 4
-  else:
-    return dp(n-3) + dp(n-2) + dp(n-1)
+dp = [0, 1, 2, 4]
+
+for i in range(4, 11):
+    dp.append(dp[i-3]+dp[i-2]+dp[i-1])
 
 for _ in range(int(input())):
-  n = int(input())
-  print(dp(n))
+    print(dp[int(input())])
 
-# for문을 통해 dp테이블에 n=10까지 값을 모두 구한 풀이도 있지만 확장성을 고려하면 안좋지않을까?
+# T가 얼마일지 몰라 메모이제이션 -> 결론은 더 느림
