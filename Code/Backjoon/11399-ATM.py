01c3bd802c9669f6a333483386271e945f2e1385
seokzin <seokzin95@gmail.com>
2021-08-27 19:29:13 +0900

    [BOJ/11399] ATM (S3)

--- Code/Backjoon/11399-ATM.py
+++ Code/Backjoon/11399-ATM.py
@@ -1,13 +1,10 @@
 n = int(input())
+p = sorted(list(map(int, input().split())))
+res = 0
 
-result = 0
-p = list(map(int, input().split()))
+for i in range(1, n+1):
+  res += sum(p[0 : i])
 
-p.sort()
+print(res)
 
-for i in range(n):
-  result += sum(p[0 : i+1])
-
-print(result)
-
-# 4라인 - list 안붙이면 어떤 결과가 나오길래?
+# SJF 스케줄링과 비슷
