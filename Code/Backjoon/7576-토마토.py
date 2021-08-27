cea5e878bcd47652bd965591b23b58d2a8f0ac6e
seokzin <seokzin95@gmail.com>
2021-08-27 15:24:00 +0900

    [BOJ/7576] 토마토 (S1)

--- Code/Backjoon/7576-토마토.py
+++ Code/Backjoon/7576-토마토.py
@@ -1,40 +1,35 @@
 from collections import deque
 
-m, n = map(int, input().split())
-
-matrix = []
-queue = deque()
-
-dx = [1, -1, 0, 0]
-dy = [0, 0, -1, 1]
-
-for _ in range(n):
-  matrix.append(list(map(int, input().split())))
-
 def bfs():
-  while queue:
-    x, y = queue.popleft()
-    for i in range(4):
-      nx = x + dx[i]
-      ny = y + dy[i]
-      if 0 <= nx < n and 0 <= ny < m and matrix[nx][ny] == 0:
-        matrix[nx][ny] = matrix[x][y] + 1
-        queue.append([nx, ny])
+    D = ((1, 0), (0, 1), (-1, 0), (0, -1))
+    q = deque()
 
-for i in range(n):
-  for j in range(m):
-    if matrix[i][j] == 1:
-      queue.append([i, j])
+    for i in range(n):
+        for j in range(m):
+            if arr[i][j] == 1:
+                q.append([i, j])
+
+    while q:
+        x, y = q.popleft()
+
+        for d in D:
+            nx, ny = x+d[0], y+d[1]
+            
+            if 0 <= nx < n and 0 <= ny < m and arr[nx][ny] == 0:
+                arr[nx][ny] = arr[x][y] + 1
+                q.append([nx, ny])
+
+
+m, n = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(n)]
 
 bfs()
 
-result = max(map(max, matrix)) - 1
+res = max(map(max, arr)) - 1
 
-for i in range(n):
-  for j in range(m):
-    if matrix[i][j] == 0:
-      result = -1
+for row in arr:
+    for x in row:
+        if x == 0:
+            res = - 1
 
-print(result)
-
-# 다른 풀이에 비해 장치가 많이 줄어든 코드가 과연 좋은 코드일까?? 
+print(res)
