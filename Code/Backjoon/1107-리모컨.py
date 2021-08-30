76763c9437dd2da3c4ec5cbaa24d81e6581f584d
seokzin <seokzin95@gmail.com>
2021-08-31 00:56:03 +0900

    [BOJ/1107] 리모컨 (G5)

--- Code/Backjoon/1107-리모컨.py
+++ Code/Backjoon/1107-리모컨.py
@@ -9,7 +9,8 @@ def check(num):
 
 n = int(input())
 m = int(input())
-btn = input().split()  # 고장난 버튼
+btn = input().split() if m else [] # 고장난 버튼이 있을 때만 받는다.
+
 res = abs(100 - n)  # 모든 숫자가 고장난 최악의 경우값
 
 for i in range(1000001):  # 1~5까지 고장났을 땐 6부터 내려오므로. 범위는 최대 100만이다.
