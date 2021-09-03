fd6778d06304fc7e6309fbf1b894a0679480be08
seokzin <seokzin95@gmail.com>
2021-09-03 23:55:20 +0900

    [BOJ/1992] 쿼드트리 (S1)

---
+++ Code/Backjoon/1992-쿼드트리.py
@@ -0,0 +1,28 @@
+# 분할 정복 - 쿼드트리 (4개씩 나뉨)
+def div(x, y, n):
+    global res
+
+    start = arr[y][x]  # 첫 색깔
+
+    for i in range(x, x+n):
+        for j in range(y, y+n):
+            if arr[j][i] != start: # 하나라도 틀리면 DnC
+
+                print('(', end="")
+                div(x, y, n//2)
+                div(x+n//2, y, n//2)
+                div(x, y+n//2, n//2)
+                div(x+n//2, y+n//2, n//2)
+                print(')', end="")
+
+                return
+    
+    print(start, end="")
+
+
+n = int(input())
+arr = [list(map(int, input())) for _ in range(n)]
+
+div(0, 0, n)
+
+# str.extend를 도입했다가 18줄 return none 때문에 꼬였습니다.
