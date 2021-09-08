754e677fd1677e96a00f5dfd195d8f164d4fe0ba
seokzin <seokzin95@gmail.com>
2021-09-09 00:01:37 +0900

    [BOJ/16928] 뱀과 사다리 게임 (S1)

---
+++ Code/Backjoon/16928-뱀과 사다리 게임.py
@@ -0,0 +1,30 @@
+from collections import deque
+
+n, m = map(int, input().split())
+arr = [*range(101)]  # 이런 방법도 가능
+visit = [0] * 101
+
+for _ in range(n+m) :
+    x, y = map(int, input().split())
+    arr[x] = y
+
+
+def bfs():
+    q = deque()
+    q.append(1)
+
+    while q :
+        x = q.popleft()
+
+        for i in range(1, 7) :
+            if x+i <= 100:
+                nx = arr[x+i]  # 사다리, 뱀 체크
+
+                if not visit[nx]:
+                    visit[nx] = visit[x]+1
+                    q.append(nx)
+
+
+bfs()
+
+print(visit[-1])
