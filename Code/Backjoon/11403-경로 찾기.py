abe4d9e4a36467c1fc9491236ee643494a5e366e
seokzin <seokzin95@gmail.com>
2021-09-08 23:53:56 +0900

    [BOJ/11403] 경로 찾기 (S1)

---
+++ Code/Backjoon/11403-경로 찾기.py
@@ -0,0 +1,13 @@
+n = int(input())
+graph = [list(map(int, input().split())) for _ in range(n)]
+
+for k in range(n):
+    for a in range(n):
+        for b in range(n):
+            if graph[a][k] and graph[k][b]:
+                graph[a][b] = 1
+
+for row in graph:
+    print(*row)
+
+# DFS가 더 쉽지만 플로이드-워셜을 flag 처럼 풀이
