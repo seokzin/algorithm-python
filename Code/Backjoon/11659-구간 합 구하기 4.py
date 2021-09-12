0ca9841f1e9057e2536d165daf846d125d2f640a
seokzin <seokzin95@gmail.com>
2021-09-12 19:06:47 +0900

    [BOJ/11659] 구간 합 구하기 4 (S3)

--- Code/Backjoon/11659-구간 합 구하기 4.py
+++ Code/Backjoon/11659-구간 합 구하기 4.py
@@ -3,18 +3,11 @@ input = sys.stdin.readline
 
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
-sum_arr = [0] * (n+1) # 부분합, 인덱스 보정
+presum = [0] * (n+1)  # 부분합
 
-sum_arr[1] = arr[0] # n=1일 때 할당
-
-for i in range(1, n):
-  sum_arr[i+1] = sum_arr[i] + arr[i]
+for i in range(n):
+    presum[i+1] = presum[i] + arr[i]
 
 for _ in range(m):
-  i, j = map(int, input().split())
-  if i == 1:
-    print(sum_arr[j])
-  else:
-    print(sum_arr[j] - sum_arr[i-1])
-
-# sys.readline 안쓰면 틀린다.
+    i, j = map(int, input().split())
+    print(presum[j] - presum[i-1])
