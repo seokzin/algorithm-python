5dde1b96e56caac64fef15df96a58b3106a28448
seokzin <seokzin95@gmail.com>
2021-09-12 19:07:52 +0900

    refac: [BOJ/17626] Fix max_cnt

--- Code/Backjoon/17626-Four Squares.py
+++ Code/Backjoon/17626-Four Squares.py
@@ -2,7 +2,7 @@ n = int(input())
 dp = [0, 1]
 
 for i in range(2, n+1):
-    cnt = 50000
+    cnt = 4  # 최대로 초기화
 
     for j in range(1, int(i**0.5)+1):
         cnt = min(cnt, dp[i - (j**2)])
@@ -11,5 +11,4 @@ for i in range(2, n+1):
 
 print(dp[-1])
 
-# bottom-up DP - 제곱 단위로 탐색
-# Python3 시간 초과
+# bottom-up DP - 결국은 제곱 단위로 모두 탐색
