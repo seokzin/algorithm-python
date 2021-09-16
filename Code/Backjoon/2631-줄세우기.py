fa1c98acff24a9a5b4085212a33ac84576ead416
seokzin <seokzin95@gmail.com>
2021-09-16 15:16:16 +0900

    [BOJ/2631] 줄세우기 (G5)

---
+++ Code/Backjoon/2631-줄세우기.py
@@ -0,0 +1,13 @@
+n = int(input())
+arr = [int(input()) for _ in range(n)]
+
+dp = [1] * n
+
+for i in range(n):
+    for j in range(i):
+        if arr[i] > arr[j]:
+            dp[i] = max(dp[i], dp[j]+1)
+
+print(n - max(dp))
+
+# LIS와 동일
