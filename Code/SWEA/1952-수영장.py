894ab3135c1f54447c5a3fd548259c1fb322f7e6
seokzin <seokzin95@gmail.com>
2021-09-24 15:23:04 +0900

    [SWEA/1952] 수영장 (dp)

--- Code/SWEA/1952-수영장.py
+++ Code/SWEA/1952-수영장.py
@@ -1,22 +1,12 @@
-def solve(cost, m):
-    global res
-
-    if m > 12:
-        res = min(res, cost)
-        return
-
-    # 1일권, 1달권
-    solve(cost + min(a*arr[m], b), m+1)
-
-    # 3달권
-    solve(cost + c, m+3)
-
-
 for tc in range(1, int(input())+1):
     a, b, c, d = map(int, input().split())  # 1일, 1달, 3달, 1년
     arr = [0] + list(map(int, input().split()))
-    res = d  # 1년치로 초기화
+    dp = [0] * 13
 
-    solve(0, 1)
-    
-    print(f"#{tc} {res}")
+    dp[1] = min(a*arr[1], b)
+    dp[2] = dp[1] + min(a*arr[2], b)
+
+    for i in range(3, 13):
+        dp[i] = min(dp[i-3] + c, dp[i-1] + b, dp[i-1] + arr[i]*a)
+
+    print(f"#{tc}", min(dp[-1], d))
