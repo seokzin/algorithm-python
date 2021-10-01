7c75740936fe14cf488b44b34c707bb4e2714e78
seokzin <seokzin95@gmail.com>
2021-10-01 13:49:57 +0900

    [SWEA/1966] 숫자를 정렬하자 (refac)

--- Code/SWEA/1966-숫자를 정렬하자.py
+++ Code/SWEA/1966-숫자를 정렬하자.py
@@ -1,8 +1,16 @@
-T = int(input())
+def selection_sort(s):
+    if s:
+        x = min(s)
+        s.remove(x)
+        return [x] + selection_sort(s)
+    else:
+        return []
 
-for tc in range(1, T+1):
-    N = int(input())
+
+for tc in range(1, int(input())+1):
+    n = int(input())
     arr = list(map(int, input().split()))
 
-    print(f"#{tc} ", end="")
-    print(*sorted(arr))
+    print(f'#{tc}', *selection_sort(arr))
+
+# 재귀적 선택정렬 직접 구현해봄
