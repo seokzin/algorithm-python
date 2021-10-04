7527507e847c760c3ae3f24a25c42b23e1c7b917
seokzin <seokzin95@gmail.com>
2021-10-04 21:25:52 +0900

    [SWEA/5208] 전기버스2

---
+++ Code/SWEA/5208-전기버스2.py
@@ -0,0 +1,23 @@
+def dfs(x, cnt):
+    global res
+
+    if res < cnt:
+        return
+
+    if x >= n:
+        res = cnt
+        return
+
+
+    for i in range(arr[x-1], 0, -1):  # 역탐색 중요
+        dfs(x+i, cnt+1)
+
+
+for tc in range(1, int(input())+1):
+    n, *arr = list(map(int, input().split()))
+    res = float('inf')
+    dfs(1, -1)  # 출발은 카운팅 X라서 -1 시작
+
+    print(f'#{tc}', res)
+
+# DP로 풀고 싶었는데 전기연료 개념 때문에 어렵
