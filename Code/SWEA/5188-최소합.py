e509743be2e67a1c9d24ec9679a36eca90ea7659
seokzin <seokzin95@gmail.com>
2021-10-05 09:45:23 +0900

    [SWEA/5188] 최소합 (refac)

--- Code/SWEA/5188-최소합.py
+++ Code/SWEA/5188-최소합.py
@@ -1,30 +1,24 @@
-def dfs(x, y, r, d, s):  # 우, 하 횟수,
+def dfs(x, y, s):
     global res
 
     if s >= res:
         return
 
-    s += arr[y][x]
+    if x+y == 2*(n-1):
+        res = s
+        return
 
-    if r+d == 2*(n-1):
-        res = min(res, s)
-
-    else:
-        if r < n-1:
-            dfs(x+1, y, r+1, d, s)
-
-        if d < n-1:
-            dfs(x, y+1, r, d+1, s)
+    if x < n-1:
+        dfs(x+1, y, s+arr[y][x+1])
+    if y < n-1:
+        dfs(x, y+1, s+arr[y+1][x])
 
 
 for tc in range(1, int(input())+1):
     n = int(input())
     arr = [list(map(int, input().split())) for _ in range(n)]
-    res = 9999
+    res = float('inf')
 
-    dfs(0, 0, 0, 0, 0)
-
+    dfs(0, 0, arr[0][0])
+    
     print(f'#{tc}', res)
-
-# 우, 하 이동 횟수가 n-1 로 고정되어있어 가능한 풀이
-# s >= res시 break 없으면 시간 초과
