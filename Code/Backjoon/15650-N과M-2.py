c6aa47388e40eb951e194b2a4337494f0f83649d
seokzin <seokzin95@gmail.com>
2021-11-28 11:30:00 +0900

    [BOJ/15650] N과 M (2) (S3)

--- Code/Backjoon/15650-N과M-2.py
+++ Code/Backjoon/15650-N과M-2.py
@@ -1,18 +1,15 @@
+def dfs(x):
+    if len(s) == m:
+        print(*s)
+        return
+
+    for i in range(x, n+1):
+        s.append(i)
+        dfs(i+1)
+        s.pop()
+
+
 n, m = map(int, input().split())
 s = []
 
-def dfs(start):
-  if len(s) == m:
-    print(*s)
-    return
-  
-  for i in range(start, n+1):
-    # if i not in s:
-    s.append(i)
-    dfs(i+1)
-    s.pop()
-
 dfs(1)
-
-# 15649와 비슷하니 묶어서 생각
-# not in은 if visited와 비슷한 기능. 다만 중복 값은 동일 취급되는 단점이 있다.
