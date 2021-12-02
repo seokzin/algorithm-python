2a23a8196825318a8bd83a2561fcbc81de5fb07b
seokzin <seokzin95@gmail.com>
2021-12-02 15:59:13 +0900

    [BOJ/1149] RGB 거리 (S1)

--- Code/Backjoon/1149-RGB거리-1.py
+++ Code/Backjoon/1149-RGB거리-1.py
@@ -2,14 +2,13 @@ n = int(input())
 
 dp = [] 
 
-
 for _ in range(n):
-  dp.append(list(map(int, input().split())))
+    dp.append(list(map(int, input().split())))
 
 for i in range(1, n):
-  dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + dp[i][0]
-  dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + dp[i][1]
-  dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + dp[i][2]
+    dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + dp[i][0]
+    dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + dp[i][1]
+    dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + dp[i][2]
 
 print(min(dp[n-1]))
 
--- Code/Backjoon/1149-RGB거리-2.py
+++ Code/Backjoon/1149-RGB거리-2.py
@@ -1,17 +1,17 @@
 n = int(input())
 
 rgb = []
-dp = [[0]*3 for i in range(n+1)]
+dp = [[0]*3 for _ in range(n+1)]
 
 for _ in range(n):
-  cost = list(map(int, input().split()))
-  rgb.append(cost)
+    cost = list(map(int, input().split()))
+    rgb.append(cost)
 
 for i in range(1, n+1):
-  dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + rgb[i-1][0]
-  dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + rgb[i-1][1]
-  dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + rgb[i-1][2]
+    dp[i][0] = min(dp[i-1][1], dp[i-1][2]) + rgb[i-1][0]
+    dp[i][1] = min(dp[i-1][0], dp[i-1][2]) + rgb[i-1][1]
+    dp[i][2] = min(dp[i-1][0], dp[i-1][1]) + rgb[i-1][2]
 
 print(min(dp[n]))
 
-# 가독성을 위해 dp와 rgb를 개별 분리했다. 
+# 가독성을 위해 dp와 rgb를 개별 분리했다.
